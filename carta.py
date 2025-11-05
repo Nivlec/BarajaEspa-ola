b9f05b9f9f725227c71719b5ea102cb26c88ea3f
2btc10 <2btc10@iesramonllull.local>
2025-11-05 09:47:51 +0100

    second  commit

--- carta.py
+++ carta.py
@@ -1,1 +1,8 @@
 #Clase carta
+
+class Carta:
+    def __init__(self, palo, valor): #construir
+        self.palo = palo
+        self.valor = valor
+    def __repr__(self): #método para mostrar la carta
+       return f"{self.valor} de {self.palo}" 
---
+++ main.py
@@ -0,0 +1,7 @@
+from carta import Carta
+
+NuevaCarta = Carta("Corazones","As")
+print(NuevaCarta)
+
+NuevaCarta2 = Carta("Corazones", 8)
+print(NuevaCarta2)
